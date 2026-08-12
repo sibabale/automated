a0208e5b534c7fff250bd4bb83fd9d1f92286db6
Sibabale <sibabale.joja@oldmutual.com>
2026-08-12 18:27:37 +0200

    chore(app): wire ReduxProvider and StyledThemeProvider into root layout
    - Wrap children with ReduxProvider and StyledThemeProvider in layout.tsx
    - Add designs/v1/ directory with v1 design reference assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import ReduxProvider from "../redux/provider";
+import StyledThemeProvider from "../theme/provider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,7 +25,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ReduxProvider>
+          <StyledThemeProvider>{children}</StyledThemeProvider>
+        </ReduxProvider>
+      </body>
     </html>
   );
 }
