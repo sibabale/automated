bff45341e5f13da3ff8cbbdc9d6ac45d32fad461
Sibabale <sibabale.joja@oldmutual.com>
2026-08-15 10:21:24 +0200

    refactor(backend): align not-found-handler.ts to file template structure

--- backend/src/middleware/not-found-handler.ts
+++ backend/src/middleware/not-found-handler.ts
@@ -1,10 +1,20 @@
+// [ BACKEND > MIDDLEWARE > NOT FOUND HANDLER ] ######################################################
+
+// 1.1. EXTERNAL DEPENDENCIES ........................................................................
 import type { RequestHandler } from "express";
+// 1.1. END ..........................................................................................
 
+// 1.2. INTERNAL DEPENDENCIES ........................................................................
 import { HttpError } from "../errors/http-error.js";
+// 1.2. END ..........................................................................................
 
+// 1.3. MIDDLEWARE ...................................................................................
 /**
  * Produces a consistent failure for routes that were not registered.
  */
 export const notFoundHandler: RequestHandler = (request, _response, next) => {
   next(new HttpError(404, `Route ${request.method} ${request.path} was not found`));
 };
+// 1.3. END ..........................................................................................
+
+// END FILE ##########################################################################################
